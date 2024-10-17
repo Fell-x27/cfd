c781c070a1782d41936562d09fddbbeba47ddae8
Fell-x27 <fell-x27@mail.ru>
2024-10-17 18:48:26 +0500

    Small error handling fix;

--- scripts/common-helpers.sh
+++ scripts/common-helpers.sh
@@ -108,10 +108,10 @@ function get-version-from-path {
 function wrap-cli-command {
     local COMMAND=$1
     output=$("$COMMAND" "${@:2}" 2>&1)
-    if echo "$output" | grep -q "does not exist ("; then
+    if echo "$output" | grep -q "does not exist (Connection refused)"; then
         echo -e "\e[1;41mERROR\e[1;m Can't connect to the Cardano node. Please, check if it launched." 1>&2
         echo "" 1>&2
-        exit 1
+         exit 1
     elif [ -n "$output" ]; then        
         echo -e "$output"
     fi    
